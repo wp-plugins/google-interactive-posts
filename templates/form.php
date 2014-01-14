f57ad934b876e12b134616e98ea6ed936ed1724d
Dejan SEO <Dejan SEO@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-14 05:09:30 +0000

    Upload.
    git-svn-id: https://plugins.svn.wordpress.org/google-interactive-posts/trunk@838142 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/form.php
@@ -0,0 +1,4 @@
+<form name="GIP_home" method="post">
+    <b>API key:</b> <input value="<?php echo $api_key;?>" type="text" name="api_key">
+    <input type="submit" name="save" value="Save">
+</form>
